468319395e7954d6ac6988d0a12be47dbb9f2a97
hwacdx <hwacdx@163.com>
2021-10-11 00:18:31 +0800

    防止矩形的坐标越界

--- H211009SwipeRectangle/ViewController.m
+++ H211009SwipeRectangle/ViewController.m
@@ -5,6 +5,10 @@
 //  Created by houwei2021 on 2021/10/9.
 //
 
+//屏幕尺寸
+#define kScreenWidth [[UIScreen mainScreen] bounds].size.width
+#define kScreenHeight [[UIScreen mainScreen] bounds].size.height
+
 #import "ViewController.h"
 #import "H211009SwipeRectangle-Swift.h"
 
@@ -16,6 +20,9 @@
 
 @property (strong, nonatomic) UIView *dragView;
 
+@property (nonatomic) CGFloat dragViewWidth;
+@property (nonatomic) CGFloat dragViewHeight;
+
 @end
 
 @implementation ViewController
@@ -26,6 +33,9 @@
     
     self.view.backgroundColor = [UIColor lightGrayColor];
     
+    self.dragViewWidth = 150;
+    self.dragViewHeight = 100;
+    
     [self addSubviews];
     [self addRecognizer];
 }
@@ -34,7 +44,7 @@
     
     self.dragView = [[UIView alloc] init];
     self.dragView.backgroundColor = [UIColor yellowColor];
-    self.dragView.frame = CGRectMake(100, 100, 100, 100);
+    self.dragView.frame = CGRectMake(100, 100, self.dragViewWidth, self.dragViewHeight);
     self.dragView.layer.cornerRadius = 3;
     self.dragView.layer.masksToBounds = YES;
     [self.view addSubview:self.dragView];
@@ -53,14 +63,28 @@
 - (void)panGestureRecognizerAction:(UIPanGestureRecognizer *)recognizer {
     
     if (recognizer.state == UIGestureRecognizerStateChanged) {
-            CGPoint translation = [recognizer translationInView:self.dragView];
- 
- 
-        self.dragView.center = CGPointMake(self.dragView.center.x + translation.x, self.dragView.center.y + translation.y);
+        CGPoint translation = [recognizer translationInView:self.dragView];
         
-            //关键:不设为零会不断递增，视图会突然不见
-            [recognizer setTranslation:CGPointZero inView:self.view];
+        CGFloat tempX = self.dragView.center.x + translation.x;
+        CGFloat tempY = self.dragView.center.y + translation.y;
+        CGFloat tempWidth = self.dragViewWidth / 2;
+        CGFloat tempHeight = self.dragViewHeight / 2;
+        
+        //防止 x 坐标越界
+        if (tempX <= tempWidth || tempX > kScreenWidth - tempWidth) {
+            return;
         }
+        
+        //防止 y 坐标越界
+        if (tempY <= tempHeight || tempY > kScreenHeight - tempHeight) {
+            return;
+        }
+        
+        self.dragView.center = CGPointMake(tempX, tempY);
+        
+        //关键:不设为零会不断递增，视图会突然不见
+        [recognizer setTranslation:CGPointZero inView:self.view];
+    }
 }
 
 //- (void)swipeGestureRecognizerAction:(UISwipeGestureRecognizer *)recognizer {
